651e9bc7ca6afbe459829796463db90a203dcd00
BIANCA MEDINA <bianca.medina+GrupoSEB@seb.com.br>
2026-08-05 14:48:29 -0300

    config: models

---
+++ src/models/settings/database_connection_handler.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+CONNECTION_STRING = "sqlite+aiosqlite:///schema.db" # onde está o banco de dados
+
+# mecanismo de comunicação com o banco de dados
+engine = create_async_engine(
+    CONNECTION_STRING,
+    echo=False,
+    pool_size=2, # não criar mais do que 2 conexões simultâneas com o banco de dados
+    max_overflow=0,
+    pool_timeout=30
+    )
+
+# cria uma sessão para interagir com o banco de dados
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+class DBConnectionHandler:
+    def __init__(self) -> None: # construtor
+        self.session: Optional[AsyncSession] = None
+
+    async def __aenter__(self):
+        self.session = async_session() # cria uma nova sessão
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.close()
