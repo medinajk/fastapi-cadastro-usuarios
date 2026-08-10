36336a73c156e376c3c0c45cb6ed514e04313229
BIANCA MEDINA <bianca.medina+GrupoSEB@seb.com.br>
2026-08-10 09:39:26 -0300

    feat: implementing UserRegister

---
+++ src/controllers/user_finder.py
@@ -0,0 +1,13 @@
+from src.models.repositories.interfaces.users_repository import UsersRepositoryInterface
+
+class UserFinder:
+    def __init__(self, users_repository: UsersRepositoryInterface):
+        self.users_repository = users_repository
+
+    async def find_user_by_name(self, user_name: str) -> dict:
+        users = await self.users_repository.get_users_by_name(user_name)
+        return {
+            "type": "USERS",
+            "count": len(users),
+            "attributes": users
+        }
